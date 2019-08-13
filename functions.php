d8188f22df8f1a40bceeb2bccdebd632fa5b7b5f
Jessi Shank <jessishank1@gmail.com>
2019-08-13 07:25:03 -0700

    created child theme

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jessi
+ * Date: 2019-08-13
+ * Time: 07:17
+ */
+
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'hello-elementor';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+?>
