1ed11a9ab7140baa6c9ac534faed4aa85ad63ff3
Marianne Feng <feng.marianne@gmail.com>
2019-08-27 16:00:45 -0700

    started work on pagination, filtering works. Not finished w/ pagination though

--- custom-widgets/portfolio-overview.php
+++ custom-widgets/portfolio-overview.php
@@ -84,6 +84,167 @@ class Portfolio_Overview extends Widget_Base {
 			]
 		);
 
+		$this->add_control(
+			'portfolio_items_per_page',
+			[
+				'label' => __( 'Portfolio Items Per Page' ),
+				'type' => Controls_Manager::NUMBER,
+				'min' => 1,
+				'max' => 50,
+				'step' => 1,
+				'default' => 9,
+                'separator' => 'after',
+			]
+		);
+
+		/////portfolio link styling/////
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name' => 'portfolio_link_typography',
+				'label' => __( 'Portfolio Link Typography' ),
+				'selector' => '{{WRAPPER}} .portfolio-link',
+			]
+		);
+
+		$this->add_control(
+			'portfolio_link_color',
+			[
+				'label' => __( 'Portfolio Link Color' ),
+				'type' => Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => Scheme_Color::get_type(),
+					'value' => Scheme_Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .portfolio-link' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'portfolio_link_color_opacity',
+			[
+				'label' => __( 'Portfolio Link Color Opacity' ),
+				'type' => Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 1,
+						'step' => .01
+					],
+				],
+				'default' => [
+					'size' => .5,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .portfolio-link' => 'opacity: {{SIZE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'portfolio_link_hover_color',
+			[
+				'label' => __( 'Portfolio Link Hover Color' ),
+				'type' => Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => Scheme_Color::get_type(),
+					'value' => Scheme_Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} a.portfolio-link:hover' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'portfolio_link_hover_color_opacity',
+			[
+				'label' => __( 'Portfolio Link Hover Color Opacity' ),
+				'type' => Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 1,
+						'step' => .01
+					],
+				],
+				'default' => [
+					'size' => 1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} a.portfolio-link:hover' => 'opacity: {{SIZE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'portfolio_link_active_color',
+			[
+				'label' => __( 'Portfolio Link Active Color' ),
+				'type' => Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => Scheme_Color::get_type(),
+					'value' => Scheme_Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} a.active.portfolio-link' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'portfolio_link_active_color_opacity',
+			[
+				'label' => __( 'Portfolio Link Active Color Opacity' ),
+				'type' => Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 1,
+						'step' => .01
+					],
+				],
+				'default' => [
+					'size' => 1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} a.active.portfolio-link' => 'opacity: {{SIZE}};',
+				],
+			]
+		);
+
+		////pagination controls////
+		$this->add_control(
+			'pagination_alignment',
+			[
+				'label' => __( 'Pagination Alignment' ),
+				'type' => Controls_Manager::SELECT,
+				'separator' => 'before',
+				'options' => [
+					'left' => 'Left',
+					'center' => 'Center',
+					'right' => 'Right',
+				],
+				'label_block' => true,
+				'default' => 'center',
+			]
+		);
+
+		$this->add_control(
+			'pagination_item_padding',
+			[
+				'label' => __( 'Pagination Item Padding' ),
+				'type' => Controls_Manager::DIMENSIONS,
+				'label_block' => true,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .portfolio-pagination a' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
 		$this->end_controls_section();
 
 		$this->start_controls_section(
@@ -163,15 +324,6 @@ class Portfolio_Overview extends Widget_Base {
 			]
 		);
 
-		$this->add_group_control(
-			Group_Control_Typography::get_type(),
-			[
-				'name' => 'portfolio_filters_typography',
-				'label' => __( 'Portfolio Filters Typography' ),
-				'selector' => '{{WRAPPER}} .portfolio-header .portfolio-filters a',
-			]
-		);
-
 		$this->add_control(
 			'portfolio_filters_margin',
 			[
@@ -181,116 +333,6 @@ class Portfolio_Overview extends Widget_Base {
 				'size_units' => [ 'px', '%', 'em' ],
 				'selectors' => [
 					'{{WRAPPER}} .portfolio-header .portfolio-filters a' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-				],
-			]
-		);
-
-		$this->add_control(
-			'portfolio_filters_color',
-			[
-				'label' => __( 'Portfolio Filters Color' ),
-				'type' => Controls_Manager::COLOR,
-				'scheme' => [
-					'type' => Scheme_Color::get_type(),
-					'value' => Scheme_Color::COLOR_1,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .portfolio-header .portfolio-filters a' => 'color: {{VALUE}}',
-				],
-			]
-		);
-
-		$this->add_control(
-			'portfolio_filters_color_opacity',
-			[
-				'label' => __( 'Portfolio Filters Color Opacity' ),
-				'type' => Controls_Manager::SLIDER,
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 1,
-						'step' => .01
-					],
-				],
-				'default' => [
-					'size' => .5,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .portfolio-header .portfolio-filters a' => 'opacity: {{SIZE}};',
-				],
-			]
-		);
-
-		$this->add_control(
-			'portfolio_filters_hover_color',
-			[
-				'label' => __( 'Portfolio Filters Hover Color' ),
-				'type' => Controls_Manager::COLOR,
-				'separator' => 'before',
-				'scheme' => [
-					'type' => Scheme_Color::get_type(),
-					'value' => Scheme_Color::COLOR_1,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .portfolio-header .portfolio-filters a:hover' => 'color: {{VALUE}}',
-				],
-			]
-		);
-
-		$this->add_control(
-			'portfolio_filters_hover_color_opacity',
-			[
-				'label' => __( 'Portfolio Filters Hover Color Opacity' ),
-				'type' => Controls_Manager::SLIDER,
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 1,
-						'step' => .01
-					],
-				],
-				'default' => [
-					'size' => 1,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .portfolio-header .portfolio-filters a:hover' => 'opacity: {{SIZE}};',
-				],
-			]
-		);
-
-		$this->add_control(
-			'portfolio_filters_active_color',
-			[
-				'label' => __( 'Portfolio Filters Active Color' ),
-				'type' => Controls_Manager::COLOR,
-				'separator' => 'before',
-				'scheme' => [
-					'type' => Scheme_Color::get_type(),
-					'value' => Scheme_Color::COLOR_1,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .portfolio-header .portfolio-filters a.active' => 'color: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->add_control(
-			'portfolio_filters_active_color_opacity',
-			[
-				'label' => __( 'Portfolio Filters Active Color Opacity' ),
-				'type' => Controls_Manager::SLIDER,
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 1,
-						'step' => .01
-					],
-				],
-				'default' => [
-					'size' => 1,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .portfolio-header .portfolio-filters a.active' => 'opacity: {{SIZE}};',
 				],
 			]
 		);
@@ -411,10 +453,10 @@ class Portfolio_Overview extends Widget_Base {
 		$visible_filters = [];
 		foreach($settings['portfolio_filters'] as $visible_filter_slug) {
 			$filter_details = get_category_by_slug( $visible_filter_slug );
-			$visible_filters[] = "<a href='#$visible_filter_slug'>$filter_details->name</a>";
+			$visible_filters[] = "<a class='portfolio-link' href='#$visible_filter_slug'>$filter_details->name</a>";
 		}
 
-		$visible_filters[] = '<a class="active" href="#all">All</a>';
+		$visible_filters[] = '<a class="portfolio-link active" href="#all">All</a>';
 
 		$posts = get_posts(array(
 			'numberposts'   =>  -1
@@ -429,7 +471,11 @@ class Portfolio_Overview extends Widget_Base {
 		foreach($posts as $portfolio_item) {
 			$post_featured_img = get_the_post_thumbnail_url($portfolio_item->ID);
 			$post_link = get_permalink($portfolio_item->ID);
-			$portfolio_html = "<div class='portfolio-item elementor-column elementor-col-$item_column_val' style='flex-direction: column;'>
+
+			$post_categories = get_the_category($portfolio_item->ID);
+            $post_category_slugs = implode(' ', array_map(function($c) { return $c->slug; }, $post_categories));
+
+			$portfolio_html = "<div class='portfolio-item elementor-column all elementor-col-$item_column_val $post_category_slugs' style='flex-direction: column;'>
 					<a href='$post_link' class='portfolio-item-link'>
 						<div class='portfolio-item-img'>
 							<img src='$post_featured_img'>
@@ -445,6 +491,7 @@ class Portfolio_Overview extends Widget_Base {
 		}
 
 		$labels_flex_display = $settings['portfolio_labels_display'];
+		$pagination_align = $settings['pagination_alignment'];
 
 		echo "<div class='portfolio-header' style='display:flex; align-items: center; justify-content: $labels_flex_display'>
 				<$portfolio_header_tag class='portfolio-header-text'>$portfolio_header_text</$portfolio_header_tag>
@@ -453,11 +500,67 @@ class Portfolio_Overview extends Widget_Base {
 		echo "	</div>
 			  </div><div class='portfolio-items elementor-row'>";
 		echo implode('', $portfolio_items);
-		echo "</div>";
+		echo "</div><div class='portfolio-pagination' style='text-align: $pagination_align'></div>";
+
 		?>
 		<script type="text/javascript">
-			jQuery(document).ready(function() {
-				console.log('marianne');
+			jQuery(document).ready(function($) {
+				console.log('hot damn this is so much code');
+                var visible = "";
+
+                $('div.portfolio-filters').on('click', 'a', function (event) {
+                    visible = '.' + this.href.slice(this.href.indexOf("#") + 1);
+
+                    $('div.portfolio-filters a').removeClass('active');
+                    $(this).addClass('active');
+                    pagination();
+                    event.preventDefault();
+                });
+
+                var totalItems = $('div.portfolio-item').length;
+                var itemsPerPage = <?= $settings['portfolio_items_per_page']?>;
+                var min = 0;
+                var max = itemsPerPage;
+
+                function pagination(action) {
+                    var totalItems = $("div.portfolio-item" + visible).length;
+
+                    if (max < totalItems) {//Stop action if max reaches more than total items
+                        //TODO: change this to be page number click
+                        if (action == "next") {
+
+                            min = min + itemsPerPage;
+                            max = max + itemsPerPage;
+
+                        }
+                    }
+
+                    if (min > 0) {//Stop action if min reaches less than 0
+                        //TODO: change this to be page number click
+                        if (action == "prev") {
+
+                            min = min - itemsPerPage;
+                            max = max - itemsPerPage;
+
+                        }
+                    }
+
+                    $("div.portfolio-item").hide();
+                    $("div.portfolio-item" + visible).slice(min, max).show();
+                }
+
+                pagination();
+
+                if (Math.floor(totalItems / itemsPerPage) > 0) {
+                    $( "div.portfolio-pagination" ).append("<span class='portfolio-link'><</span>");
+                    for (var i = 1; i <= Math.floor(totalItems / itemsPerPage); i++) {
+                        let isActive = i === 1 ? 'active' : '';
+                        var paginationLink = "<a href='#' class='portfolio-link " + isActive + "' id='" + i + "'>" + i + "</a>";
+                        console.log('inside of pagination printing loop', paginationLink);
+                        $( "div.portfolio-pagination" ).append( paginationLink );
+                    }
+                    $( "div.portfolio-pagination" ).append("<span class='portfolio-link' >></span>");
+                }
 
 			});
 		</script>
