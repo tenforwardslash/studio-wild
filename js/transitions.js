f56fda40d95664ef5a138c60babab6f107bb853f
Marianne Feng <feng.marianne@gmail.com>
2020-01-22 12:47:04 -0800

    fixed bug with sometimes slide in effect not appearing on mobile

--- js/transitions.js
+++ js/transitions.js
@@ -5,10 +5,10 @@
         var rect = $(this)[0].getBoundingClientRect();
 
         return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+            Math.round(rect.top) >= 0 &&
+            Math.round(rect.left) >= 0 &&
+            Math.round(rect.bottom) <= $(window).height() &&
+            Math.round(rect.right) <= $(window).width()
         );
     };
 
